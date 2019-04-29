552eafdedd705b1ef3d114d506870e52a855ae88
pengyue <wei.yi@hipac.cn>
2019-04-29 14:18:38 +0800

    two sum

---
+++ src/main/java/com/yiwei/arithmetic/s001/TwoSum0.java
@@ -0,0 +1,28 @@
+package com.yiwei.arithmetic.s001;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum0 {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int[] res = twoSum(nums,17);
+        for(int i : res){
+            System.out.print(i + " ");
+        }
+    }
+}
